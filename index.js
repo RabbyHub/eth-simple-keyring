d850a23bc8199416bd6dfdc065e49b40d8991255
vvvvvv1vvvvvv <galennnnnnn@gmail.com>
2021-10-22 16:08:11 +0800

    fix: revert to 4.2.0 code

--- index.js
+++ index.js
@@ -1,171 +1,191 @@
 const { EventEmitter } = require('events');
+const Wallet = require('ethereumjs-wallet').default;
 const ethUtil = require('ethereumjs-util');
-const randomBytes = require('randombytes');
 
 const type = 'Simple Key Pair';
-const {
-  concatSig,
-  decrypt,
-  getEncryptionPublicKey,
-  normalize,
-  personalSign,
-  signTypedData,
-  SignTypedDataVersion,
-} = require('@metamask/eth-sig-util');
-
-function generateKey() {
-  const privateKey = randomBytes(32);
-  // I don't think this is possible, but this validation was here previously,
-  // so it has been preserved just in case.
-  // istanbul ignore next
-  if (!ethUtil.isValidPrivate(privateKey)) {
-    throw new Error(
-      'Private key does not satisfy the curve requirements (ie. it is invalid)',
-    );
-  }
-  return privateKey;
-}
+const sigUtil = require('eth-sig-util');
 
 class SimpleKeyring extends EventEmitter {
   constructor(opts) {
     super();
     this.type = type;
-    this._wallets = [];
+    this.wallets = [];
     this.deserialize(opts);
   }
 
-  async serialize() {
-    return this._wallets.map(({ privateKey }) => privateKey.toString('hex'));
+  serialize() {
+    return Promise.resolve(
+      this.wallets.map((w) => w.getPrivateKey().toString('hex')),
+    );
   }
 
-  async deserialize(privateKeys = []) {
-    this._wallets = privateKeys.map((hexPrivateKey) => {
-      const strippedHexPrivateKey = ethUtil.stripHexPrefix(hexPrivateKey);
-      const privateKey = Buffer.from(strippedHexPrivateKey, 'hex');
-      const publicKey = ethUtil.privateToPublic(privateKey);
-      return { privateKey, publicKey };
+  deserialize(privateKeys = []) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.wallets = privateKeys.map((privateKey) => {
+          const stripped = ethUtil.stripHexPrefix(privateKey);
+          const buffer = Buffer.from(stripped, 'hex');
+          const wallet = Wallet.fromPrivateKey(buffer);
+          return wallet;
+        });
+      } catch (e) {
+        reject(e);
+      }
+      resolve();
     });
   }
 
-  async addAccounts(n = 1) {
+  addAccounts(n = 1) {
     const newWallets = [];
     for (let i = 0; i < n; i++) {
-      const privateKey = generateKey();
-      const publicKey = ethUtil.privateToPublic(privateKey);
-      newWallets.push({ privateKey, publicKey });
+      newWallets.push(Wallet.generate());
     }
-    this._wallets = this._wallets.concat(newWallets);
-    const hexWallets = newWallets.map(({ publicKey }) =>
-      ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)),
+    this.wallets = this.wallets.concat(newWallets);
+    const hexWallets = newWallets.map((w) =>
+      ethUtil.bufferToHex(w.getAddress()),
     );
-    return hexWallets;
+    return Promise.resolve(hexWallets);
   }
 
-  async getAccounts() {
-    return this._wallets.map(({ publicKey }) =>
-      ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)),
+  getAccounts() {
+    return Promise.resolve(
+      this.wallets.map((w) => ethUtil.bufferToHex(w.getAddress())),
     );
   }
 
   // tx is an instance of the ethereumjs-transaction class.
-  async signTransaction(address, tx, opts = {}) {
-    const privKey = this._getPrivateKeyFor(address, opts);
+  signTransaction(address, tx, opts = {}) {
+    const privKey = this.getPrivateKeyFor(address, opts);
     const signedTx = tx.sign(privKey);
     // Newer versions of Ethereumjs-tx are immutable and return a new tx object
-    return signedTx === undefined ? tx : signedTx;
+    return Promise.resolve(signedTx === undefined ? tx : signedTx);
   }
 
   // For eth_sign, we need to sign arbitrary data:
-  async signMessage(address, data, opts = {}) {
+  signMessage(address, data, opts = {}) {
     const message = ethUtil.stripHexPrefix(data);
-    const privKey = this._getPrivateKeyFor(address, opts);
+    const privKey = this.getPrivateKeyFor(address, opts);
     const msgSig = ethUtil.ecsign(Buffer.from(message, 'hex'), privKey);
-    const rawMsgSig = concatSig(msgSig.v, msgSig.r, msgSig.s);
-    return rawMsgSig;
+    const rawMsgSig = sigUtil.concatSig(msgSig.v, msgSig.r, msgSig.s);
+    return Promise.resolve(rawMsgSig);
+  }
+
+  // For eth_sign, we need to sign transactions:
+  newGethSignMessage(withAccount, msgHex, opts = {}) {
+    const privKey = this.getPrivateKeyFor(withAccount, opts);
+    const msgBuffer = ethUtil.toBuffer(msgHex);
+    const msgHash = ethUtil.hashPersonalMessage(msgBuffer);
+    const msgSig = ethUtil.ecsign(msgHash, privKey);
+    const rawMsgSig = sigUtil.concatSig(msgSig.v, msgSig.r, msgSig.s);
+    return Promise.resolve(rawMsgSig);
   }
 
   // For personal_sign, we need to prefix the message:
-  async signPersonalMessage(address, msgHex, opts = {}) {
-    const privKey = this._getPrivateKeyFor(address, opts);
-    const privateKey = Buffer.from(privKey, 'hex');
-    const sig = personalSign({ privateKey, data: msgHex });
-    return sig;
+  signPersonalMessage(address, msgHex, opts = {}) {
+    const privKey = this.getPrivateKeyFor(address, opts);
+    const privKeyBuffer = Buffer.from(privKey, 'hex');
+    const sig = sigUtil.personalSign(privKeyBuffer, { data: msgHex });
+    return Promise.resolve(sig);
   }
 
   // For eth_decryptMessage:
-  async decryptMessage(withAccount, encryptedData) {
+  decryptMessage(withAccount, encryptedData) {
     const wallet = this._getWalletForAccount(withAccount);
-    const privateKey = ethUtil.stripHexPrefix(wallet.privateKey);
-    const sig = decrypt({ privateKey, encryptedData });
-    return sig;
+    const privKey = ethUtil.stripHexPrefix(wallet.getPrivateKey());
+    const sig = sigUtil.decrypt(encryptedData, privKey);
+    return Promise.resolve(sig);
   }
 
   // personal_signTypedData, signs data along with the schema
-  async signTypedData(
-    withAccount,
-    typedData,
-    opts = { version: SignTypedDataVersion.V1 },
-  ) {
-    // Treat invalid versions as "V1"
-    const version = Object.keys(SignTypedDataVersion).includes(opts.version)
-      ? opts.version
-      : SignTypedDataVersion.V1;
+  signTypedData(withAccount, typedData, opts = { version: 'V1' }) {
+    switch (opts.version) {
+      case 'V1':
+        return this.signTypedData_v1(withAccount, typedData, opts);
+      case 'V3':
+        return this.signTypedData_v3(withAccount, typedData, opts);
+      case 'V4':
+        return this.signTypedData_v4(withAccount, typedData, opts);
+      default:
+        return this.signTypedData_v1(withAccount, typedData, opts);
+    }
+  }
 
-    const privateKey = this._getPrivateKeyFor(withAccount, opts);
-    return signTypedData({ privateKey, data: typedData, version });
+  // personal_signTypedData, signs data along with the schema
+  signTypedData_v1(withAccount, typedData, opts = {}) {
+    const privKey = this.getPrivateKeyFor(withAccount, opts);
+    const sig = sigUtil.signTypedDataLegacy(privKey, { data: typedData });
+    return Promise.resolve(sig);
+  }
+
+  // personal_signTypedData, signs data along with the schema
+  signTypedData_v3(withAccount, typedData, opts = {}) {
+    const privKey = this.getPrivateKeyFor(withAccount, opts);
+    const sig = sigUtil.signTypedData(privKey, { data: typedData });
+    return Promise.resolve(sig);
+  }
+
+  // personal_signTypedData, signs data along with the schema
+  signTypedData_v4(withAccount, typedData, opts = {}) {
+    const privKey = this.getPrivateKeyFor(withAccount, opts);
+    const sig = sigUtil.signTypedData_v4(privKey, { data: typedData });
+    return Promise.resolve(sig);
   }
 
   // get public key for nacl
-  async getEncryptionPublicKey(withAccount, opts = {}) {
-    const privKey = this._getPrivateKeyFor(withAccount, opts);
-    const publicKey = getEncryptionPublicKey(privKey);
-    return publicKey;
+  getEncryptionPublicKey(withAccount, opts = {}) {
+    const privKey = this.getPrivateKeyFor(withAccount, opts);
+    const publicKey = sigUtil.getEncryptionPublicKey(privKey);
+    return Promise.resolve(publicKey);
   }
 
-  _getPrivateKeyFor(address, opts = {}) {
+  getPrivateKeyFor(address, opts = {}) {
     if (!address) {
       throw new Error('Must specify address.');
     }
     const wallet = this._getWalletForAccount(address, opts);
-    return wallet.privateKey;
+    const privKey = ethUtil.toBuffer(wallet.getPrivateKey());
+    return privKey;
   }
 
   // returns an address specific to an app
-  async getAppKeyAddress(address, origin) {
+  getAppKeyAddress(address, origin) {
     if (!origin || typeof origin !== 'string') {
       throw new Error(`'origin' must be a non-empty string`);
     }
-    const wallet = this._getWalletForAccount(address, {
-      withAppKeyOrigin: origin,
+    return new Promise((resolve, reject) => {
+      try {
+        const wallet = this._getWalletForAccount(address, {
+          withAppKeyOrigin: origin,
+        });
+        const appKeyAddress = sigUtil.normalize(
+          wallet.getAddress().toString('hex'),
+        );
+        return resolve(appKeyAddress);
+      } catch (e) {
+        return reject(e);
+      }
     });
-    const appKeyAddress = normalize(
-      ethUtil.publicToAddress(wallet.publicKey).toString('hex'),
-    );
-    return appKeyAddress;
   }
 
   // exportAccount should return a hex-encoded private key:
-  async exportAccount(address, opts = {}) {
+  exportAccount(address, opts = {}) {
     const wallet = this._getWalletForAccount(address, opts);
-    return wallet.privateKey.toString('hex');
+    return Promise.resolve(wallet.getPrivateKey().toString('hex'));
   }
 
   removeAccount(address) {
     if (
-      !this._wallets
-        .map(({ publicKey }) =>
-          ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)).toLowerCase(),
-        )
+      !this.wallets
+        .map((w) => ethUtil.bufferToHex(w.getAddress()).toLowerCase())
         .includes(address.toLowerCase())
     ) {
       throw new Error(`Address ${address} not found in this keyring`);
     }
 
-    this._wallets = this._wallets.filter(
-      ({ publicKey }) =>
-        ethUtil
-          .bufferToHex(ethUtil.publicToAddress(publicKey))
-          .toLowerCase() !== address.toLowerCase(),
+    this.wallets = this.wallets.filter(
+      (w) =>
+        ethUtil.bufferToHex(w.getAddress()).toLowerCase() !==
+        address.toLowerCase(),
     );
   }
 
@@ -173,22 +193,20 @@ class SimpleKeyring extends EventEmitter {
    * @private
    */
   _getWalletForAccount(account, opts = {}) {
-    const address = normalize(account);
-    let wallet = this._wallets.find(
-      ({ publicKey }) =>
-        ethUtil.bufferToHex(ethUtil.publicToAddress(publicKey)) === address,
+    const address = sigUtil.normalize(account);
+    let wallet = this.wallets.find(
+      (w) => ethUtil.bufferToHex(w.getAddress()) === address,
     );
     if (!wallet) {
       throw new Error('Simple Keyring - Unable to find matching address.');
     }
 
     if (opts.withAppKeyOrigin) {
-      const { privateKey } = wallet;
+      const privKey = wallet.getPrivateKey();
       const appKeyOriginBuffer = Buffer.from(opts.withAppKeyOrigin, 'utf8');
-      const appKeyBuffer = Buffer.concat([privateKey, appKeyOriginBuffer]);
-      const appKeyPrivateKey = ethUtil.keccak(appKeyBuffer, 256);
-      const appKeyPublicKey = ethUtil.privateToPublic(appKeyPrivateKey);
-      wallet = { privateKey: appKeyPrivateKey, publicKey: appKeyPublicKey };
+      const appKeyBuffer = Buffer.concat([privKey, appKeyOriginBuffer]);
+      const appKeyPrivKey = ethUtil.keccak(appKeyBuffer, 256);
+      wallet = Wallet.fromPrivateKey(appKeyPrivKey);
     }
 
     return wallet;
